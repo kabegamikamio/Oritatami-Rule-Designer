46dfcf9a7c4986f0109e4abe8fbf1b697c3af685
kabegamikamio <kabegamikamio@gmail.com>
2024-11-06 23:33:10 +0900

    start to implement countBond()

--- src/ruledesigner/main/Oritatami.java
+++ src/ruledesigner/main/Oritatami.java
@@ -5,11 +5,13 @@ import java.util.List;
 public class Oritatami {
     final private int delay;
     final private boolean isOblivious;
+    private BondingRule bondingRule;
     private Point[][] grid;
 
-    Oritatami(int delay, boolean isOblivious) {
-        this.delay = delay;
+    Oritatami(int delay, boolean isOblivious, BondingRule bondingRule) {
+        this.delay = 3;
         this.isOblivious = isOblivious;
+        this.bondingRule = bondingRule;
     }
 
     /**
@@ -26,15 +28,14 @@ public class Oritatami {
         this.grid = new Point[length * 2][length * 2];
 
         if(this.isOblivious) {
-            return executeOritatamiOblivious(transcript, bondingRule, initialConformation);
+            return executeOritatamiOblivious(transcript, initialConformation);
         } else {
-            return executeOritatamiInertial(transcript, bondingRule, initialConformation);
+            return executeOritatamiInertial(transcript, initialConformation);
         }
     }
 
     // execute Oritatami simulation in the oblivious dynamics
-    private Conformation executeOritatamiOblivious(Transcript transcript, BondingRule bondingRule,
-                                                   Conformation initialConformation) {
+    private Conformation executeOritatamiOblivious(Transcript transcript, Conformation initialConformation) {
         // produced conformation
         Conformation conformation = new Conformation();
 
@@ -72,8 +73,12 @@ public class Oritatami {
     // Energy minimumising is done by maximising the number of bonds
     private Conformation findEnergyMinimumConformation(Conformation currentConformation, Bead[] beads) {
         Conformation conformation = new Conformation();
-        int minimumEnergy = 0;
-        Point[] newPoints = new Point[3];
+
+        int maxBonds = 0;
+        int[] maxDirection = new int[this.delay];
+
+        int bonds;
+        Point[] newPoints = new Point[this.delay];
 
         // obtain the coordination of the point at the end of the current conformation
         Point terminalPoint = currentConformation.getPoint(-1);
@@ -84,7 +89,13 @@ public class Oritatami {
                 for (int k = 0; k < 6; k++) {
                     newPoints[0] = terminalPoint.getAdjacentOf(i);
                     newPoints[1] = newPoints[0].getAdjacentOf(j);
-                    newPoints[2] = newPoints[1].getAdjacecntOf(k);
+                    newPoints[2] = newPoints[1].getAdjacentOf(k);
+
+                    bonds = countBonds();
+
+                    if(bonds > maxBonds) {
+                        conformation = conformation.add();
+                    }
                 }
             }
         }
@@ -94,12 +105,29 @@ public class Oritatami {
 
     // evaluate the number of bonds
     // in developing this method, be sure to refer the formalization of the oblivious dynamics...
-    private int countBonds() {}
+    private int countBonds(Conformation conformation, Bead[] beads, Point[] points) {
+        Conformation newConformation = new Conformation();
+        Point point;
+        Bead bead;
+        Bond partialBond;
+        int bond = 0;
+
+        for (int i = 0; i < conformation.getLength(); i++) {
+            point = conformation.getPoint(i);
+            bead = conformation.getBead(i);
+
+            for(int j = 0; j < this.delay; j++) {
+                partialBond = new Bond(bead, beads[j]);
+                if(points[j].isAdjacent(point) && bondingRule.ifContains(partialBond)) {
+                    bond++;
+                }
+            }
+        }
+    }
 
     // execute Oritatami simulation in the inertial dynamics
     // not available atm
-    private Conformation executeOritatamiInertial(Transcript transcript, BondingRule bondingRule,
-                                                  Conformation initialConformation) {
+    private Conformation executeOritatamiInertial(Transcript transcript, Conformation initialConformation) {
         Conformation conformation = new Conformation();
 
         // inertial dynamics process
